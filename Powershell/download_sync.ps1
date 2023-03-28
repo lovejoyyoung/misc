5d84461d10b897464e7387e1d813796be38fc785
Jinyang Liu <lovejoyyoung@gmail.com>
2023-03-27 21:30:24 -0400

    update ps1

--- Powershell/download_sync.ps1
+++ Powershell/download_sync.ps1
@@ -1,19 +1,37 @@
+#run PowerShell as adminstration
+#Get-ExecutionPolicy
+#if Restricted then run
+#Set-ExecutionPolicy RemoteSigned
+#Done
+
+Write-Host "`nHi, Which build you are gonna download? Example: xxxx"
+$main = Read-Host
+$down = (($main -Split "-")[2, 3, 4, 5] -join "-")-replace '^[^.]*\.'
+$v1 = ($main.Split('/')[1]).Split('-')[0]
+$v2 = ($v1 -split "\.")[0..1] -join "."
+
+$headers = @{"X-JFrog-Art-Api" = "YOUR_API_TOKEN"}
 $name = "PC_USER_NAME"
-$build = "BUILD_NAME"
 $hidden = "DOWNLOAD_LINK"
+$normal = "UNENCRYPTED_LINK"
 
 $urls = @(
-    "https://$hidden/$build/downloadfile1.zip",
-    "https://$hidden/$build/downloadfile2.zip",
-    "https://$hidden/$build/downloadfile3.zip",
-    "https://$hidden/$build/downloadfile4.zip"
+    "https://$hidden/$main/$v1.zip",
+    "https://$hidden/$main/$v1.zip",
+    "https://$hidden/$down/$v2.0.zip",
+    "https://$hidden/$down/$v2.0.zip"
 )
 
 Set-Location "C:\Users\$name\Downloads"
-
 Get-Date -Format "'Download Started' HH:mm:ss"
 foreach ($url in $urls){
     $fileName = Split-Path $url -Leaf
-    Invoke-WebRequest -Uri $url -Headers $headers -OutFile $fileName
+    try {
+        Invoke-WebRequest -Uri $url -Headers $headers -OutFile $fileName
+    }catch (
+        Write-Host "- $fileName is failed to download."
+    )
 }
 Get-Date -Format "'Download Finished' HH:mm:ss"
+Write-Output "`nThe downstream apps are downloaded from the below link"
+Write-Output "$normal/$down/XXXX/XXXX"
